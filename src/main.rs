b2c10e39b97fd704a85b268818cf5f3ff4e20e21
Verademora <drhax123@gmail.com>
2023-08-22 01:56:34 -0500

    Cleanup with rustfmt and clippy

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
-use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn Error>> {
     // Open our input file and read it to a String
     let mut file = File::open("src/input")?;
     let mut buffer = String::new();
@@ -14,12 +14,10 @@ fn main() -> Result<(), Box<dyn Error>>{
     // Recollect all sums into a new collection
     let mut sums: Vec<i32> = Vec::new();
     for elf in v {
-        let v2: Vec<i32> = elf.split('\n')
-            .filter_map(|s| s.parse().ok())
-            .collect();
+        let v2: Vec<i32> = elf.split('\n').filter_map(|s| s.parse().ok()).collect();
         sums.push(v2.iter().sum());
     }
-    
+
     // Sort by largest and collect the top 3
     sums.sort_by(|a, b| b.cmp(a));
     let top_three_sum: i32 = sums.iter().take(3).sum();
